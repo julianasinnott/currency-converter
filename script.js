336cfd20746ce33288f3343b6aed1f653bcb8ddb
Juliana Sinnott <julianasinnott@outlook.com>
2022-07-28 22:17:55 -0300

    refactor
     converter data fix, input validation improvements, fixed two decimal places

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const {real, dollar, euro} = form
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  console.log(e)
   getData()
 })
 
 function getData() {
   fetch('https://economia.awesomeapi.com.br/last/BRL-USD,USD-BRL,EUR-BRL,BRL-EUR,EUR-USD,USD-EUR')
   .then(response => {
-    return  response.json()
+    return response.json()
   })
   .then (data => {
     setData(data)
@@ -17,17 +18,26 @@ function getData() {
 }
 
 function setData(data) {
-  if(real.value != '') {
-    dollar.placeholder = (data.USDBRL.ask)*real.value
-    euro.placeholder = (data.EURBRL.ask)*real.value
+  if (real.value != '' && (!(real.value).includes(','))) {
+    dollar.placeholder = ((data.BRLUSD.ask)*real.value).toFixed(2)
+    euro.placeholder = ((data.BRLEUR.ask)*real.value).toFixed(2)
+  } 
+  validationInputs(real.value)
+  if (dollar.value != '' && (!(dollar.value).includes(','))) {
+    real.placeholder = ((data.USDBRL.ask)*dollar.value).toFixed(2)
+    euro.placeholder = ((data.USDEUR.ask)*dollar.value).toFixed(2)
   }
-  if (dollar.value != '') {
-    real.placeholder = (data.BRLUSD.ask)*dollar.value
-    euro.placeholder = (data.EURUSD.ask)*dollar.value
-  }
-  if (euro.value != '') {
-    real.placeholder = (data.BRLEUR.ask)*euro.value
-    dollar.placeholder = (data.USDEUR.ask)*euro.value
+  validationInputs(dollar.value)
+  if (euro.value != '' && (!(dollar.value).includes(','))) {
+    real.placeholder = ((data.EURBRL.ask)*euro.value).toFixed(2)
+    dollar.placeholder = ((data.EURUSD.ask)*euro.value).toFixed(2)
+  } 
+  validationInputs(euro.value)
+}
+
+function validationInputs(value) {
+  if (value != '' && value.includes(',')) {
+    alert('Utilize ponto (.) ao invés de vírgulas (,)')
   }
 }
 
